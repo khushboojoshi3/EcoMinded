0f1a9f19e9cb0ff76480d6c2bd49b4e64d3dd65f
dioiio <108053898+dioiio@users.noreply.github.com>
2023-02-27 19:33:34 +0530

    new pages added by a pro designer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Shop from './pages/Shop/Shop';
 import HomePage from './pages/HomePage/HomePage';
 import Quiz from './pages/Quiz/Quiz';
 import Home from './pages/HomePage/HomePage';
+import Store from './pages/Store/Store';
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
    <Route path="/electricity" element={<Electricity/>} />
    <Route path='/shop' element={<Shop/>} />
    <Route path='/quiz' element={<Quiz/>}/>
+   <Route path='/store' element={<Store/>}/>
       </Routes>
     </BrowserRouter>
   );
